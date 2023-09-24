e2d665b534e406c2ac9be677a765785aebcea359
Dhruvil Rathod <drathod@asite.com>
2023-09-24 18:35:07 +0530

    Optimized: deleting .angular folder and angular dependencies entirely after build to reduce size of docker image

--- package-modifier.js
+++ package-modifier.js
@@ -5,15 +5,19 @@ let contents = fs.readFileSync(path.join(__dirname, 'package.json'));
 contents = JSON.parse(contents.toString());
 
 let dependenciesKeys = Object.keys(contents.dependencies);
-console.log(dependenciesKeys);
+let devDependenciesKeys = Object.keys(contents.devDependencies);
 
-for (var i = 0; i < dependenciesKeys.length; i++) {
-    if (dependenciesKeys[i].includes("@angular/")) {
-        contents.devDependencies[dependenciesKeys[i]] = contents.dependencies[dependenciesKeys[i]];
+for (let i = 0; i < dependenciesKeys.length; i++) {
+    if (dependenciesKeys[i].includes("@angular")) {
         delete contents.dependencies[dependenciesKeys[i]];
     }
 }
 
+for (let i = 0; i < devDependenciesKeys.length; i++) {
+    if (devDependenciesKeys[i].includes("@angular")) {
+        delete contents.devDependencies[devDependenciesKeys[i]];
+    }
+}
 fs.writeFile(path.join(__dirname, 'package.json'), JSON.stringify(contents), (err) => {
     if(err) throw err;
     else console.log('package.json optimized');
--- start_server.sh
+++ start_server.sh
@@ -1,5 +1,12 @@
 echo "starting application with NodeJS version: $(node --version)"
 WORK_DIR_PATH="/usr/main/sample-angular-nest/"
+NODE_MODULE_SIZE=$(du -hs ""$WORK_DIR_PATH"node_modules")
+ANGULAR_CACHE_SIZE=$(du -hs ""$WORK_DIR_PATH".angular")
+DIRECTORY_TOTAL_SIZE=$(du -hs "$WORK_DIR_PATH")
+
+echo "NODE_MODULE_SIZE: $NODE_MODULE_SIZE"
+echo "ANGULAR_CACHE_SIZE: $ANGULAR_CACHE_SIZE"
+echo "DIRECTORY_TOTAL_SIZE: $DIRECTORY_TOTAL_SIZE"
 
 (cd "$WORK_DIR_PATH" && node "builds/backend/main.js") &
 (cd / && nginx -g "daemon off;")
